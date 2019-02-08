9d2f8cefae2fac3ccc4b922c3e7dd16cff241688
Wojciech Olejnik <divoolej@gmail.com>
2019-02-08 19:02:44 +0100

    Update Picobu to 1.0.4

--- picobu.rb
+++ picobu.rb
@@ -1,16 +1,10 @@
 class Picobu < Formula
-  desc "Picobu: Simple, one-command build system for PICO-8 games"
+  desc "Simple, one-command build system for PICO-8 games"
   homepage "https://github.com/Divoolej/picobu"
-  url "https://github.com/Divoolej/picobu/archive/1.0.2.tar.gz"
-  sha256 "c490aaea2bee635ab2d101da0c718d785dc3c392ff85c7bae6734f974e8ac230"
+  url "https://github.com/Divoolej/picobu/archive/1.0.4.tar.gz"
+  sha256 "d07bd4ca3d615489dec88ab2c442dc846afb6c45813877af2df4d6c37405dbe6"
 
   depends_on "rust" => :build
-
-  bottle do
-    root_url "https://github.com/Divoolej/picobu/releases/download/1.0.2/"
-    cellar :any_skip_relocation
-    sha256 "aa0c95c56809e700f47d0a2c10e5dd3accca95fb1bc55f6c8cf335b8e0c3827d" => :mojave
-  end
 
   def install
     system "cargo", "install", "--root", prefix, "--path", "."
